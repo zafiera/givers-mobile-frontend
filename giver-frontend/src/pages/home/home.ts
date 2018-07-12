6eb9ca4b46ffd73f12003f5acb99adc2ccb11bdd
Zafiera Davids <zafiera@cowboyaliens.com>
2018-07-13 00:19:14 +0200

    register and login page

--- giver-frontend/src/pages/home/home.ts
+++ giver-frontend/src/pages/home/home.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+  ) {}
 
-  constructor(public navCtrl: NavController) {
-
-  }
 
 }
---
+++ giver-frontend/src/pages/login/login.ts
@@ -0,0 +1,38 @@
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { HomePage } from "../home/home";
+import { RegisterPage } from "../register/register";
+import { Http } from "@angular/http";
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: "page-login",
+  templateUrl: "login.html"
+})
+export class LoginPage {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: Http
+  ) {}
+
+  navigateToHome() {
+    console.log("Navigating...");
+    this.navCtrl.push(HomePage);
+  }
+  navigateToRegister() {
+    console.log("Navigating...");
+    this.navCtrl.push(RegisterPage);
+  }
+  navigateToUpdate() {
+    console.log("Navigating..");
+    // this.navCtrl.push(UpdatePage);
+  }
+}
